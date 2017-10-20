e2d5752998a5c85e1536f2f9052d1ca2b9f5ab0b
Lisa Leung <110@holbertonschool.com>
2017-10-20 06:14:58 +0000

    deletes and adds

--- static/scripts/experience.js
+++ static/scripts/experience.js
@@ -1,12 +1,21 @@
 const $ = window.$;
 
-$(document).ready(function () {
+$(document).ready(function(){
+    $("#positive-add").click(function(){
+	let posVal = $('#positive').val();
+	let posScale = $('#positivescale').val();
+        let experienceadded = $('#experienceAdded');
+        let grid = $('<div/>', {class: 'col-xs-3'}).appendTo(experienceadded);
+        let icon = $('<span/>', {class: 'glyphicon glyphicon-remove-circle remove-icon'}).appendTo(grid);
+        let text = $('<span/>', {text:posVal + '(' + posScale + ')'}).appendTo(icon);
 
-    $(".remove-icon").click(function(){
-	$(this).parent().fadeOut(300);
+
+        $('#positive').val('');
+        $('#positivescale').val('');
     });
 
-    $("#positive-add").click(function(){
-	console.log("this");
+
+    $('body').on('click', '.remove-icon', function() {
+	$(this).parent().fadeOut(300);
     });
 });
