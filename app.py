5dd16a56686b1e8cd30a5916393196ef9d98e6b8
Stuart Kuredjian <108@holbertonschool.com>
2017-10-21 06:25:19 +0000

    add functionality to add a positive experience to the db

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pymysql
 from flask_cors import CORS, cross_origin
 import json
 import uuid
+from datetime import datetime
+import time
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -14,6 +16,53 @@ def mainpage():
     return render_template('login.html',
                            cache_id=uuid.uuid4()
     )
+
+@app.route('/save_exp/', methods=['GET', 'OPTIONS', 'POST'], strict_slashes=False)
+def save_exp():
+    """ inserts new symptom into 'experiences' table """
+    response = request.data.decode('utf-8')
+    obj = json.loads(response)
+
+    con = pymysql.connect('localhost', 'wellness_dev', 'wellness_dev_pwd', 'wellness_dev_db')
+    cursor = con.cursor()
+    symp_name = obj[0]['symp_name']
+    scale = obj[0]['scale']
+    if scale == '':
+        scale = 5
+    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    _type = obj[0]['type']
+    user_id = 1 #<--- this needs to be dynamic
+    count = 0
+
+    cursor.execute('SELECT `frequency` FROM `experiences` WHERE symp_name=\'{}\' ORDER BY date DESC LIMIT 1'.format(symp_name))
+    result = cursor.fetchall();
+    if not result:
+        count = 1;
+    else:
+        count += result[0][0] + 1;
+
+    cursor.execute('INSERT INTO experiences (symp_name, scale, date, type, user_id, frequency) VALUES("{}", "{}", "{}", "{}", "{}", "{}")'.format(symp_name, scale, date, _type, user_id, count))
+    con.commit()
+    con.close()
+
+# can't use until email is imported from sign in
+#    if data_exists(symp_name, date):
+#        return jsonify(True)
+    return jsonify(False)
+
+def data_exists(symp_name, date):
+    """ confirms existance of symptom entry  in 'experiences table'"""
+    con = pymysql.connect('localhost', 'wellness_dev', 'wellness_dev_pwd', 'wellness_dev_db')
+    cursor = con.cursor()
+    results = ()
+    var1 = cursor.execute("SELECT EXISTS(SELECT 1 FROM experiences WHERE symp_name='{}' AND date='{}')".format(symp_name, date))
+    print(var1)
+    results = cursor.fetchone()
+    cursor.execute("SELECT * FROM credentials WHERE email='{}'".format(email))
+    results = cursor.fetchall()
+    if results == (()):
+        return False
+    return True
 
 @app.route('/signup/', methods=['GET', 'OPTIONS', 'POST'], strict_slashes=False)
 def signup():
--- static/scripts/experience.js
+++ static/scripts/experience.js
@@ -1,34 +1,55 @@
 const $ = window.$;
 
 $(document).ready(function(){
-    let expName = [];
-    let expObj = [];
-
     /* Add positive button clicked
      * Positive Experience Button Clicked adds to build expObj*/
-    $("#positive-add").click(function(){
-	let posVal = $('#positive').val();
-	let posScale = $('#positivescale').val();
+    $("#positive-add").on('click', function () {
+		const symp_name = $('#positive').val();
+		const scale = $('#positive_scale').val();
+//		const date = new Date().toLocaleString();
+		const type = 'positive';
+		const obj = [{'symp_name': symp_name,
+					'scale':scale,
+					'type': type}]
 
-	if (posScale == "") {
-	    posScale = 5;
-	}
+		$.ajax({
+			url: 'http://localhost:5001/save_exp',
+			type: 'POST',
+			dataType: 'json',
+			contentType: 'application/json',
+			data: JSON.stringify(obj),
+			success: function (res) {
+				console.log(res)
+			},
+			error: function (res) {
+				console.error('Error: ' + res);
+			}
+		});
 
-	/*checks if the experience was already added in the current session already*/
-	if (!expName.includes(posVal) && posVal != "") {
-	    /*Dynamically building the DOM*/
-            let experienceadded = $('#experienceAdded');
-            let grid = $('<div/>', {class: 'col-xs-3'}).appendTo(experienceadded);
+	});
+
+/*    $("#positive-add").click(function(){
+		let posVal = $('#positive').val();
+		let posScale = $('#positivescale').val();
+
+		if (posScale == "") {
+			posScale = 5;
+		}
+		/*checks if the experience was already added in the current session already*/
+/*		if (!expName.includes(posVal) && posVal != "") {
+			/*Dynamically building the DOM*/
+    /*        let experienceAdded = $('#experienceAdded');
+            let grid = $('<div/>', {class: 'col-xs-3'}).appendTo(experienceAdded);
             let icon = $('<span/>', {class: 'glyphicon glyphicon-remove-circle remove-icon'}).appendTo(grid);
             let text = $('<span/>', {class: 'symdisplay', id:posVal+posScale, name: posVal, text:posVal + '(' + posScale + ')'}).appendTo(icon);
-	    /*adding the experience to expObj*/
-	    expName.push(posVal);
+			/*adding the experience to expObj*/
+	/*		expName.push(posVal);
             $('#positive').val('');
-	    $('#positivescale').val('');
-	    newPos = {'name': posVal,'scale': posScale, 'type': 'pos'};
-	    expObj.push(newPos);
-	}
-    });
+			$('#positivescale').val('');
+			newPos = {'name': posVal,'scale': posScale, 'type': 'pos'};
+			expObj.push(newPos);;
+		}
+    }); */
     /* Add negative button clicked
      * Negative Experience Button Clicked adds to build expObj*/
     $("#negative-add").click(function(){
@@ -41,8 +62,8 @@ $(document).ready(function(){
 	/*checks if the experience was already added in the current session already*/
         if (!expName.includes(negVal) && negVal != "") {
 	    /*Dynamically building the DOM*/
-            let experienceadded = $('#experienceAdded');
-            let grid = $('<div/>', {class: 'col-xs-3'}).appendTo(experienceadded);
+            let experienceAdded = $('#experienceAdded');
+            let grid = $('<div/>', {class: 'col-xs-3'}).appendTo(experienceAdded);
             let icon = $('<span/>', {class: 'glyphicon glyphicon-remove-circle remove-icon'}).appendTo(grid);
             let text = $('<span/>', {class: 'symdisplay', id:negVal + negScale, name: negVal, text:negVal + '(' + negScale + ')'}).appendTo(icon);
 	    /*adding the experience to expObj*/
