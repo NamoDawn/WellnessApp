fa43a14673df0b216c3a08a2e5a89b13847c552a
Ubuntu <ubuntu@ip-172-31-28-51.us-west-1.compute.internal>
2017-10-28 06:51:41 +0000

    10_27_17_wellness_dev_db_dump.sql :add 'USE wellness_dev_db'

--- db_dump/10_27_17_wellness_dev_db_dump.sql
+++ db_dump/10_27_17_wellness_dev_db_dump.sql
@@ -19,6 +19,7 @@
 -- Table structure for table `credentials`
 --
 
+USE wellness_dev_db
 DROP TABLE IF EXISTS `credentials`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
