58c42ead64b2c5fdcba3b0bd4332f2313a5b2c48
root <root@ubuntu-512mb-sfo2-01.localdomain>
2017-11-08 08:46:08 +0000

    modularize and comments

--- aws/us-east-1/autodeploy.py
+++ aws/us-east-1/autodeploy.py
@@ -9,6 +9,7 @@ from fabric.api import local, run, hosts, env, put
 env.hosts = []
 
 def install_dependencies():
+    """ installing all the dependencies necessary for wellness app"""
     run("sudo apt-get update")
     run("sudo apt-get install -y nginx")
     run("sudo service nginx restart")
@@ -22,11 +23,23 @@ def install_dependencies():
     run("sudo pip install flask-cors")
     run("sudo pip install passlib")
     run("sudo pip install pymysql")
+
+def setup_nginx():
+    """ setting up nginx"""
     run("sudo rm /etc/nginx/sites-available/default")
     run("sudo git clone https://github.com/NamoDawn/WellnessApp.git /home/ubuntu")
     run("sudo wget https://gist.githubusercontent.com/NamoDawn/be33e53528e4cf3001f6e3b1f53ad3ef/raw/845f0cd3af78a5b7e2c11ddbc6c345a08deb9232/default -P /etc/nginx/sites-available/")
     run("sudo chmod 755 /etc/nginx/sites-available/default")
+    run("sudo service nginx restart")
+
+def setup_upstart():
+    """ configuring the upstart script"""
     run("sudo wget https://gist.githubusercontent.com/NamoDawn/0ce751fcdd44587d7e594128a1770450/raw/8647dcd2793d9cd891182406c18ae89c7fd89b53/wellness.conf -P /etc/init/")
     run("sudo chmod 755 /etc/init/wellness.conf")
-    run("sudo start /etc/init/wellness")
-    run("sudo service nginx restart")
+    with cd('/etc/init/'):
+        run('sudo start wellness')
+
+def deploy():
+    install_dependencies()
+    setup_nginx()
+    setup_upstart()
