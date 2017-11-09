ae3817770dc1e6cce3bae541bfd3e52d7f51dc8a
root <110@holbertonschool.com>
2017-11-09 06:07:57 +0000

    adding csv files and adding wellness locally

--- aws/us-east-1/autodeploy.py
+++ aws/us-east-1/autodeploy.py
@@ -6,7 +6,7 @@ import os
 import time
 from fabric.api import local, run, hosts, env, put, cd
 
-env.hosts = ['54.166.197.222']
+env.hosts = ['54.211.234.124']
 
 def install_dependencies():
     """ installing all the dependencies necessary for wellness app"""
@@ -26,7 +26,7 @@ def setup_nginx():
     run("sudo rm -f /etc/nginx/sites-available/default")
 
     run("sudo git -C /home/ubuntu clone https://github.com/NamoDawn/WellnessApp.git")
-    run("sudo wget https://gist.githubusercontent.com/NamoDawn/be33e53528e4cf3001f6e3b1f53ad3ef/raw/845f0cd3af78a5b7e2c11ddbc6c345a08deb9232/default -P /etc/nginx/sites-available/")
+    put('wellness.conf', '/etc/init/', use_sudo=True)
     run("sudo chmod 755 /etc/nginx/sites-available/default")
     run("sudo service nginx restart")
 
@@ -35,9 +35,18 @@ def setup_upstart():
     run("sudo wget https://gist.githubusercontent.com/NamoDawn/0ce751fcdd44587d7e594128a1770450/raw/25da0c533eecac46de42d3ebc7f9972e13320f4e/wellness.conf -P /etc/init/")
     run("sudo chmod 755 /etc/init/wellness.conf")
 
+def create_csv():
+    run("sudo touch /home/ubuntu/WellnessApp/static/data/everything.csv")
+    run("sudo chmod 777 /home/ubuntu/WellnessApp/static/data/everything.csv")
+    run("sudo touch /home/ubuntu/WellnessApp/static/data/week.csv")
+    run("sudo chmod 777 /home/ubuntu/WellnessApp/static/data/week.csv") 
+    run("sudo touch /home/ubuntu/WellnessApp/static/data/month.csv")
+    run("sudo chmod 777 /home/ubuntu/WellnessApp/static/data/month.csv")
+
 def deploy():
     install_dependencies()
     setup_upstart()
     setup_nginx()
     with cd('/etc/init'):
         run('sudo start wellness')
+    create_csv()
