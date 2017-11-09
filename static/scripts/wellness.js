47bf00cc4237a501d330ee713fc86a8f6e3e42ba
root <110@holbertonschool.com>
2017-11-09 07:47:04 +0000

    user validation for front end

--- static/scripts/wellness.js
+++ static/scripts/wellness.js
@@ -7,7 +7,36 @@ $(document).ready(function () {
     const fName = $('#fName').val();
     const lName = $('#lName').val();
     signup(email, password, fName, lName);
+    modal.style.display = "block";
   });
+
+  /*Modal Script*/
+
+    // Get the modal
+    var modal = document.getElementById('signupModal');
+    // Get the button that opens the modal
+    var btn = document.getElementById("signup_button");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    //btn.onclick = function() {
+    //modal.style.display = "block";
+    //}
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+	modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+	if (event.target == modal) {
+            modal.style.display = "none";
+	}
+    }
+
   /* Signin event listener */
   $('#signin_button').on('click', function () {
     const email = $('#si_email').val();
@@ -99,10 +128,17 @@ $(document).ready(function () {
       contentType: 'application/json',
       data: JSON.stringify(creds),
       success: function (res) {
-        return true;
+	  $("#signup_notification").html("Sign Up Successful!");
+          $("#f_name").val("");
+          $("#l_name").val("");
+          $("#email").val("");
+          $("#password").val("");
+	  return true;
       },
       error: function (res) {
         console.error('error with signup:' + res);
+        $("#signup_notification").html("Failed! Sign Up Was Not Successful!");
+
         return false;
       }
     });
