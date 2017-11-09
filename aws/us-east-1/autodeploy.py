dd934aa1a573a44186937df71e2f137eca0e9447
root <110@holbertonschool.com>
2017-11-09 06:19:53 +0000

    reverting nginx config file

--- aws/us-east-1/autodeploy.py
+++ aws/us-east-1/autodeploy.py
@@ -6,7 +6,7 @@ import os
 import time
 from fabric.api import local, run, hosts, env, put, cd
 
-env.hosts = ['54.211.234.124']
+env.hosts = ['34.226.154.101']
 
 def install_dependencies():
     """ installing all the dependencies necessary for wellness app"""
@@ -21,18 +21,20 @@ def install_dependencies():
     run("sudo pip3 install pymysql")
     run("sudo apt-get install -y git")
 
+def add_project():
+    run("sudo git -C /home/ubuntu clone https://github.com/NamoDawn/WellnessApp.git")
+
+
 def setup_nginx():
     """ setting up nginx"""
     run("sudo rm -f /etc/nginx/sites-available/default")
-
-    run("sudo git -C /home/ubuntu clone https://github.com/NamoDawn/WellnessApp.git")
-    put('wellness.conf', '/etc/init/', use_sudo=True)
+    run("sudo wget https://gist.githubusercontent.com/NamoDawn/be33e53528e4cf3001f6e3b1f53ad3ef/raw/845f0cd3af78a5b7e2c11ddbc6c345a08deb9232/default -P /etc/nginx/sites-available/")
     run("sudo chmod 755 /etc/nginx/sites-available/default")
     run("sudo service nginx restart")
 
 def setup_upstart():
     """ configuring the upstart script"""
-    run("sudo wget https://gist.githubusercontent.com/NamoDawn/0ce751fcdd44587d7e594128a1770450/raw/25da0c533eecac46de42d3ebc7f9972e13320f4e/wellness.conf -P /etc/init/")
+    put('wellness.conf', '/etc/init/', use_sudo=True)
     run("sudo chmod 755 /etc/init/wellness.conf")
 
 def create_csv():
@@ -45,6 +47,7 @@ def create_csv():
 
 def deploy():
     install_dependencies()
+    add_project()
     setup_upstart()
     setup_nginx()
     with cd('/etc/init'):
