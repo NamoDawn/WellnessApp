c8d83877ad3e394fe314efad9606515624e23803
root <110@holbertonschool.com>
2017-11-10 01:30:22 +0000

    remomve aws directory, renamed to scaledeploy

--- aws/us-east-1/bash_scripts/get_instanceIP.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TAGKEY=<key>
-TAGVALUE=<value>
-
-sudo aws ec2 describe-instances --filters "Name=tag:$TAGKEY,Values=$TAGVALUE" | grep PublicIpAddress | cut -d ":" -f 2 | cut -d\
- "," -f1 | tr -d '"' | cut -d " " -f 2 > instance_ip.txt
